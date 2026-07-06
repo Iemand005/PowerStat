a779d9124b67d87ce85bfd8e1cc399c030a85762
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-07-06 16:29:12 +0200

    tab indents

--- PowerStat.psm1
+++ PowerStat.psm1
@@ -33,19 +33,19 @@ function Get-Stat {
 				$FileInfo = New-Object System.IO.FileInfo($Item.FullName)
 
 				[PSCustomObject]@{
-					Name             = $Item.Name
-					FullName         = $Item.FullName
-					Size       = if ($Item.Attributes -match "Directory") { 0 } else { $FileInfo.Length }
-					Attributes       = $Item.Attributes
-					AccessMode       = $FileInfo.Attributes
+					Name			= $Item.Name
+					FullName		= $Item.FullName
+					Size			= if ($Item.Attributes -match "Directory") { 0 } else { $FileInfo.Length }
+					Attributes		= $Item.Attributes
+					AccessMode		= $FileInfo.Attributes
 					
-					CreationTime     = $FileInfo.CreationTime
-					LastWriteTime    = $FileInfo.LastWriteTime
-					LastAccessTime   = $FileInfo.LastAccessTime
+					CreationTime	= $FileInfo.CreationTime
+					LastWriteTime	= $FileInfo.LastWriteTime
+					LastAccessTime	= $FileInfo.LastAccessTime
 					
-					CreationTicks    = $FileInfo.CreationTimeUtc.Ticks
-					LastWriteTicks   = $FileInfo.LastWriteTimeUtc.Ticks
-					LastAccessTicks  = $FileInfo.LastAccessTimeUtc.Ticks
+					CreationTicks	= $FileInfo.CreationTimeUtc.Ticks
+					LastWriteTicks	= $FileInfo.LastWriteTimeUtc.Ticks
+					LastAccessTicks	= $FileInfo.LastAccessTimeUtc.Ticks
 				}
 			} catch {
 				Write-Error "Fout bij lezen van statistieken voor $($Item.FullName): $_"
